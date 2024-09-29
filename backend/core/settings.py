c92a257d8efe48240dad3e4c83589b84d9d27b73
Cam Himes <cshimes@ncsu.edu>
2024-09-29 13:19:11 -0400

    Implemented Celery + Celery beat to grabwatch prices from Amazon once a minute

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from celery.schedules import crontab
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'watches'
+    'watches',
+    'django_celery_beat'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BEAT_SCHEDULE = {
+    "scrape_watch_prices_task": {
+        "task": "watches.tasks.scrape_watch_prices_task",
+        # Run once a minute
+        "schedule": crontab(minute="*")
+    }
+}
